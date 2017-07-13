fc667f6c75e7d3a48db0e5c6796f6dc09ed1cb23
Henry de Valence <hdevalence@cloudflare.com>
2017-07-13 16:10:30 -0700

    cln16sidh: port p-751 reduction and lazy adds from MSR assembly

--- field.go
+++ field.go
@@ -9,8 +9,18 @@ type Fp751UnreducedProduct [2 * Fp751NumWords]uint64
 /// Compute z = x + y.
 func Fp751Add(z, x, y *Fp751Element)
 
+/// Compute z = x + y, without reducing mod p.
+func Fp751LazyAdd(z, x, y *Fp751Element)
+
+/// Compute z = x + y, without reducing mod p.
+func Fp751UnreducedProductLazyAdd(z, x, y *Fp751UnreducedProduct)
+
 /// Compute z = x - y.
 func Fp751Sub(z, x, y *Fp751Element)
 
 /// Compute z = x * y.
 func Fp751Mul(z *Fp751UnreducedProduct, x, y *Fp751Element)
+
+/// Reduce an UnreducedProduct to a field element: set z = x (mod p).
+/// Destroys the input value.
+func Fp751Reduce(z *Fp751Element, x *Fp751UnreducedProduct)
