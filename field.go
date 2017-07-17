f75e7dabeaf57165f238d3b1f9cee9a954b6c3d0
Henry de Valence <hdevalence@cloudflare.com>
2017-07-17 13:21:41 -0700

    cln16sidh: Rename functions and add go:noescape
    In order to have an assembly-implemented function operate on some data, it's
    necessary to pass pointers to the data into the assembly implementation.
    However, Go sees a pointer to some data passed into a function, and cannot
    prove that the function does not keep pointers to that data.  It therefore
    assumes that the data escapes the local scope, and moves it onto the heap.  To
    avoid allocations in the hot path, instead mark the functions with
    //go:noescape, which instructs the compiler that the inputs don't escape.

--- field.go
+++ field.go
@@ -2,25 +2,33 @@ package cln16sidh
 
 const Fp751NumWords = 12
 
+// Represents an element of the base field F_p, in Montgomery form.
 type Fp751Element [Fp751NumWords]uint64
 
-type Fp751UnreducedProduct [2 * Fp751NumWords]uint64
+// Represents an intermediate product of two elements of the base field F_p.
+type Fp751X2 [2 * Fp751NumWords]uint64
 
-/// Compute z = x + y.
-func Fp751Add(z, x, y *Fp751Element)
+// Compute z = x + y (mod p).
+//go:noescape
+func Fp751AddReduced(z, x, y *Fp751Element)
 
-/// Compute z = x + y, without reducing mod p.
-func Fp751LazyAdd(z, x, y *Fp751Element)
+// Compute z = x - y (mod p).
+//go:noescape
+func Fp751SubReduced(z, x, y *Fp751Element)
 
-/// Compute z = x + y, without reducing mod p.
-func Fp751UnreducedProductLazyAdd(z, x, y *Fp751UnreducedProduct)
+// Compute z = x + y, without reducing mod p.
+//go:noescape
+func Fp751AddLazy(z, x, y *Fp751Element)
 
-/// Compute z = x - y.
-func Fp751Sub(z, x, y *Fp751Element)
+// Compute z = x + y, without reducing mod p.
+//go:noescape
+func Fp751X2AddLazy(z, x, y *Fp751X2)
 
-/// Compute z = x * y.
-func Fp751Mul(z *Fp751UnreducedProduct, x, y *Fp751Element)
+// Compute z = x * y.
+//go:noescape
+func Fp751Mul(z *Fp751X2, x, y *Fp751Element)
 
-/// Reduce an UnreducedProduct to a field element: set z = x (mod p).
-/// Destroys the input value.
-func Fp751Reduce(z *Fp751Element, x *Fp751UnreducedProduct)
+// Reduce an X2 to a field element: set z = x (mod p).
+// Destroys the input value.
+//go:noescape
+func Fp751Reduce(z *Fp751Element, x *Fp751X2)
