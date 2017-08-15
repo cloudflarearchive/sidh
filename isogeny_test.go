7c4e3b1d538380a15e15a88b65e92ef38b320066
Henry de Valence <hdevalence@cloudflare.com>
2017-08-15 15:09:27 -0700

    cln16sidh: add 3-isogeny test via Sage

--- isogeny_test.go
+++ isogeny_test.go
@@ -52,3 +52,52 @@ func TestFirstFourIsogenyVersusSage(t *testing.T) {
 		t.Error("\nExpected\n", sageIsogenized_xR.toAffine(), "\nfound\n", isogenized_xR.toAffine())
 	}
 }
+
+func TestThreeIsogenyVersusSage(t *testing.T) {
+	var xR, xP3, isogenized_xR, sageIsogenized_xR ProjectivePoint
+
+	// sage: %colors Linux
+	// sage: p = 2^372 * 3^239 - 1; Fp = GF(p)
+	//   ***   Warning: increasing stack size to 2000000.
+	//   ***   Warning: increasing stack size to 4000000.
+	// sage: R.<x> = Fp[]
+	// sage: Fp2 = Fp.extension(x^2 + 1, 'i')
+	// sage: i = Fp2.gen()
+	// sage: E0Fp = EllipticCurve(Fp, [0,0,0,1,0])
+	// sage: E0Fp2 = EllipticCurve(Fp2, [0,0,0,1,0])
+	// sage: x_PA = 11
+	// sage: y_PA = -Fp(11^3 + 11).sqrt()
+	// sage: x_PB = 6
+	// sage: y_PB = -Fp(6^3 + 6).sqrt()
+	// sage: P_A = 3^239 * E0Fp((x_PA,y_PA))
+	// sage: P_B = 2^372 * E0Fp((x_PB,y_PB))
+	// sage: def tau(P):
+	// ....:     return E0Fp2( (-P.xy()[0], i*P.xy()[1]))
+	// ....:
+	// sage: m_B = 3*randint(0,3^238)
+	// sage: R_B = E0Fp2(P_B) + m_B*tau(P_B)
+	// sage: P_3 = (3^238)*R_B
+	// sage: def three_isog(P_3, P):
+	// ....:     X3, Z3 = P_3.xy()[0], 1
+	// ....:     XP, ZP = P.xy()[0], 1
+	// ....:     x = (XP*(X3*XP - Z3*ZP)^2)/(ZP*(Z3*XP - X3*ZP)^2)
+	// ....:     A3, C3 = (Z3^4 + 9*X3^2*(2*Z3^2 - 3*X3^2)), 4*X3*Z3^3
+	// ....:     cod = EllipticCurve(Fp2, [0,A3/C3,0,1,0])
+	// ....:     return cod.lift_x(x)
+	// ....:
+	// sage: isogenized_R_B = three_isog(P_3, R_B)
+
+	xR.fromAffine(&ExtensionFieldElement{a: fp751Element{0xbd0737ed5cc9a3d7, 0x45ae6d476517c101, 0x6f228e9e7364fdb2, 0xbba4871225b3dbd, 0x6299ccd2e5da1a07, 0x38488fe4af5f2d0e, 0xec23cae5a86e980c, 0x26c804ba3f1edffa, 0xfbbed81932df60e5, 0x7e00e9d182ae9187, 0xc7654abb66d05f4b, 0x262d0567237b}, b: fp751Element{0x3a3b5b6ad0b2ac33, 0x246602b5179127d3, 0x502ae0e9ad65077d, 0x10a3a37237e1bf70, 0x4a1ab9294dd05610, 0xb0f3adac30fe1fa6, 0x341995267faf70cb, 0xa14dd94d39cf4ec1, 0xce4b7527d1bf5568, 0xe0410423ed45c7e4, 0x38011809b6425686, 0x28f52472ebed}})
+
+	xP3.fromAffine(&ExtensionFieldElement{a: fp751Element{0x7bb7a4a07b0788dc, 0xdc36a3f6607b21b0, 0x4750e18ee74cf2f0, 0x464e319d0b7ab806, 0xc25aa44c04f758ff, 0x392e8521a46e0a68, 0xfc4e76b63eff37df, 0x1f3566d892e67dd8, 0xf8d2eb0f73295e65, 0x457b13ebc470bccb, 0xfda1cc9efef5be33, 0x5dbf3d92cc02}, b: fp751Element{0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0}})
+
+	sageIsogenized_xR.fromAffine(&ExtensionFieldElement{a: fp751Element{0x286db7d75913c5b1, 0xcb2049ad50189220, 0xccee90ef765fa9f4, 0x65e52ce2730e7d88, 0xa6b6b553bd0d06e7, 0xb561ecec14591590, 0x17b7a66d8c64d959, 0x77778cecbe1461e, 0x9405c9c0c41a57ce, 0x8f6b4847e8ca7d3d, 0xf625eb987b366937, 0x421b3590e345}, b: fp751Element{0x566b893803e7d8d6, 0xe8c71a04d527e696, 0x5a1d8f87bf5eb51, 0x42ae08ae098724f, 0x4ee3d7c7af40ca2e, 0xd9f9ab9067bb10a7, 0xecd53d69edd6328c, 0xa581e9202dea107d, 0x8bcdfb6c8ecf9257, 0xe7cbbc2e5cbcf2af, 0x5f031a8701f0e53e, 0x18312d93e3cb}})
+
+	_, phi := ComputeThreeIsogeny(&xP3)
+
+	isogenized_xR = phi.Eval(&xR)
+
+	if !sageIsogenized_xR.VartimeEq(&isogenized_xR) {
+		t.Error("\nExpected\n", sageIsogenized_xR.toAffine(), "\nfound\n", isogenized_xR.toAffine())
+	}
+}
