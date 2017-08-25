73f3fb400d6071f96d85eadbf3002b253ea1e69e
Henry de Valence <hdevalence@cloudflare.com>
2017-08-25 15:32:23 -0700

    cln16sidh: move SecretPoint, DistortAndDifference to curve.go

--- curve.go
+++ curve.go
@@ -451,3 +451,175 @@ func (xR *ProjectivePoint) ThreePointLadder(curve *ProjectiveCurveParameters, xP
 	*xR = x2
 	return xR
 }
+
+// Given the affine x-coordinate affine_xP of P, compute the x-coordinate
+// x(\tau(P)-P) of \tau(P)-P.
+func DistortAndDifference(affine_xP *PrimeFieldElement) ProjectivePoint {
+	var xR ProjectivePoint
+	var t0, t1 PrimeFieldElement
+	t0.Square(affine_xP)         // = x_P^2
+	t1.One().Add(&t1, &t0)       // = x_P^2 + 1
+	xR.x.b = t1.a                // = 0 + (x_P^2 + 1)*i
+	t0.Add(affine_xP, affine_xP) // = 2*x_P
+	xR.z.a = t0.a                // = 2*x_P + 0*i
+
+	return xR
+}
+
+// Given an affine point P = (x_P, y_P) in the prime-field subgroup of the
+// starting curve E_0(F_p), together with a secret scalar m, compute x(P+[m]Q),
+// where Q = \tau(P) is the image of P under the distortion map described
+// below.
+//
+// The computation uses basically the same strategy as the
+// Costello-Longa-Naehrig implementation:
+//
+// 1. Use the standard Montgomery ladder to compute x([m]Q), x([m+1]Q)
+//
+// 2. Use Okeya-Sakurai coordinate recovery to recover [m]Q from Q, x([m]Q),
+// x([m+1]Q)
+//
+// 3. Use P and [m]Q to compute x(P + [m]Q)
+//
+// The distortion map \tau is defined as
+//
+// \tau : E_0(F_{p^2}) ---> E_0(F_{p^2})
+//
+// \tau : (x,y) |---> (-x, iy).
+//
+// The image of the distortion map is the _trace-zero_ subgroup of E_0(F_{p^2})
+// defined by Tr(P) = P + \pi_p(P) = id, where \pi_p((x,y)) = (x^p, y^p) is the
+// p-power Frobenius map.  To see this, take P = (x,y) \in E_0(F_{p^2}).  Then
+// Tr(P) = id if and only if \pi_p(P) = -P, so that
+//
+// -P = (x, -y) = (x^p, y^p) = \pi_p(P);
+//
+// we have x^p = x if and only if x \in F_p, while y^p = -y if and only if y =
+// i*y' for y' \in F_p.
+//
+// Thus (excepting the identity) every point in the trace-zero subgroup is of
+// the form \tau((x,y)) = (-x,i*y) for (x,y) \in E_0(F_p).
+//
+// Since the Montgomery ladder only uses the x-coordinate, and the x-coordinate
+// is always in the prime subfield, we can compute x([m]Q), x([m+1]Q) entirely
+// in the prime subfield.
+//
+// The affine form of the relation for Okeya-Sakurai coordinate recovery is
+// given on p. 13 of Costello-Smith:
+//
+// y_Q = ((x_P*x_Q + 1)*(x_P + x_Q + 2*a) - 2*a - x_R*(x_P - x_Q)^2)/(2*b*y_P),
+//
+// where R = Q + P and a,b are the Montgomery parameters.  In our setting
+// (a,b)=(0,1) and our points are P=Q, Q=[m]Q, P+Q=[m+1]Q, so this becomes
+//
+// y_{mQ} = ((x_Q*x_{mQ} + 1)*(x_Q + x_{mQ}) - x_{m1Q}*(x_Q - x_{mQ})^2)/(2*y_Q)
+//
+// y_{mQ} = ((1 - x_P*x_{mQ})*(x_{mQ} - x_P) - x_{m1Q}*(x_P + x_{mQ})^2)/(2*y_P*i)
+//
+// y_{mQ} = i*((1 - x_P*x_{mQ})*(x_{mQ} - x_P) - x_{m1Q}*(x_P + x_{mQ})^2)/(-2*y_P)
+//
+// since (x_Q, y_Q) = (-x_P, y_P*i).  In projective coordinates this is
+//
+// Y_{mQ}' = ((Z_{mQ} - x_P*X_{mQ})*(X_{mQ} - x_P*Z_{mQ})*Z_{m1Q}
+//          - X_{m1Q}*(X_{mQ} + x_P*Z_{mQ})^2)
+//
+// with denominator
+//
+// Z_{mQ}' = (-2*y_P*Z_{mQ}*Z_{m1Q})*Z_{mQ}.
+//
+// Setting
+//
+// X_{mQ}' = (-2*y_P*Z_{mQ}*Z_{m1Q})*X_{mQ}
+//
+// gives [m]Q = (X_{mQ}' : i*Y_{mQ}' : Z_{mQ}') with X,Y,Z all in F_p.  (Here
+// the ' just denotes that we've added extra terms to the denominators during
+// the computation of Y)
+//
+// To compute the x-coordinate x(P+[m]Q) from P and [m]Q, we use the affine
+// addition formulas of section 2.2 of Costello-Smith.  We're only interested
+// in the x-coordinate, giving
+//
+// X_R = Z_{mQ}*(i*Y_{mQ} - y_P*Z_{mQ})^2 - (x_P*Z_{mQ} + X_{mQ})*(X_{mQ} - x_P*Z_{mQ})^2
+//
+// Z_R = Z_{mQ}*(X_{mQ} - x_P*Z_{mQ})^2.
+//
+// Notice that although X_R \in F_{p^2}, we can split the computation into
+// coordinates X_R = X_{R,a} + X_{R,b}*i as
+//
+// (i*Y_{mQ} - y_P*Z_{mQ})^2 = (y_P*Z_{mQ})^2 - Y_{mQ}^2 - 2*y_P*Z_{mQ}*Y_{mQ}*i,
+//
+// giving
+//
+// X_{R,a} = Z_{mQ}*((y_P*Z_{mQ})^2 - Y_{mQ}^2)
+//         - (x_P*Z_{mQ} + X_{mQ})*(X_{mQ} - x_P*Z_{mQ})^2
+//
+// X_{R,b} = -2*y_P*Y_{mQ}*Z_{mQ}^2
+//
+// Z_R = Z_{mQ}*(X_{mQ} - x_P*Z_{mQ})^2.
+//
+// These formulas could probably be combined with the formulas for y-recover
+// and computed more efficiently, but efficiency isn't the biggest concern
+// here, since the bulk of the cost is already in the ladder.
+func SecretPoint(affine_xP, affine_yP *PrimeFieldElement, scalar []uint8) ProjectivePoint {
+	var xQ ProjectivePrimeFieldPoint
+	xQ.fromAffine(affine_xP)
+	xQ.x.Neg(&xQ.x)
+
+	// Compute x([m]Q) = (X_{mQ} : Z_{mQ}), x([m+1]Q) = (X_{m1Q} : Z_{m1Q})
+	var xmQ, xm1Q = ScalarMultPrimeField(&aPlus2Over4_E0, &xQ, scalar)
+
+	// Now perform coordinate recovery:
+	// [m]Q = (X_{mQ} : Y_{mQ}*i : Z_{mQ})
+	var XmQ, YmQ, ZmQ PrimeFieldElement
+	var t0, t1 PrimeFieldElement
+
+	// Y_{mQ} = (Z_{mQ} - x_P*X_{mQ})*(X_{mQ} - x_P*Z_{mQ})*Z_{m1Q}
+	//         - X_{m1Q}*(X_{mQ} + x_P*Z_{mQ})^2
+	t0.Mul(affine_xP, &xmQ.x)       // = x_P*X_{mQ}
+	YmQ.Sub(&xmQ.z, &t0)            // = Z_{mQ} - x_P*X_{mQ}
+	t1.Mul(affine_xP, &xmQ.z)       // = x_P*Z_{mQ}
+	t0.Sub(&xmQ.x, &t1)             // = X_{mQ} - x_P*Z_{mQ}
+	YmQ.Mul(&YmQ, &t0)              // = (Z_{mQ} - x_P*X_{mQ})*(X_{mQ} - x_P*Z_{mQ})
+	YmQ.Mul(&YmQ, &xm1Q.z)          // = (Z_{mQ} - x_P*X_{mQ})*(X_{mQ} - x_P*Z_{mQ})*Z_{m1Q}
+	t1.Add(&t1, &xmQ.x).Square(&t1) // = (X_{mQ} + x_P*Z_{mQ})^2
+	t1.Mul(&t1, &xm1Q.x)            // = X_{m1Q}*(X_{mQ} + x_P*Z_{mQ})^2
+	YmQ.Sub(&YmQ, &t1)              // = Y_{mQ}
+
+	// Z_{mQ} = -2*(Z_{mQ}^2 * Z_{m1Q} * y_P)
+	t0.Mul(&xmQ.z, &xm1Q.z).Mul(&t0, affine_yP) // = Z_{mQ} * Z_{m1Q} * y_P
+	t0.Neg(&t0)                                 // = -1*(Z_{mQ} * Z_{m1Q} * y_P)
+	t0.Add(&t0, &t0)                            // = -2*(Z_{mQ} * Z_{m1Q} * y_P)
+	ZmQ.Mul(&xmQ.z, &t0)                        // = -2*(Z_{mQ}^2 * Z_{m1Q} * y_P)
+
+	// We added terms to the denominator Z_{mQ}, so multiply them to X_{mQ}
+	// X_{mQ} = -2*X_{mQ}*Z_{mQ}*Z_{m1Q}*y_P
+	XmQ.Mul(&xmQ.x, &t0)
+
+	// Now compute x(P + [m]Q) = (X_Ra + i*X_Rb : Z_R)
+	var XRa, XRb, ZR PrimeFieldElement
+
+	XRb.Square(&ZmQ).Mul(&XRb, &YmQ) // = Y_{mQ} * Z_{mQ}^2
+	XRb.Mul(&XRb, affine_yP)         // = Y_{mQ} * y_P * Z_{mQ}^2
+	XRb.Add(&XRb, &XRb)              // = 2 * Y_{mQ} * y_P * Z_{mQ}^2
+	XRb.Neg(&XRb)                    // = -2 * Y_{mQ} * y_P * Z_{mQ}^2
+
+	t0.Mul(affine_yP, &ZmQ).Square(&t0) // = (y_P * Z_{mQ})^2
+	t1.Square(&YmQ)                     // = Y_{mQ}^2
+	XRa.Sub(&t0, &t1)                   // = (y_P * Z_{mQ})^2 - Y_{mQ}^2
+	XRa.Mul(&XRa, &ZmQ)                 // = Z_{mQ}*((y_P * Z_{mQ})^2 - Y_{mQ}^2)
+	t0.Mul(affine_xP, &ZmQ)             // = x_P * Z_{mQ}
+	t1.Add(&XmQ, &t0)                   // = X_{mQ} + x_P*Z_{mQ}
+	t0.Sub(&XmQ, &t0)                   // = X_{mQ} - x_P*Z_{mQ}
+	t0.Square(&t0)                      // = (X_{mQ} - x_P*Z_{mQ})^2
+	t1.Mul(&t1, &t0)                    // = (X_{mQ} + x_P*Z_{mQ})*(X_{mQ} - x_P*Z_{mQ})^2
+	XRa.Sub(&XRa, &t1)                  // = Z_{mQ}*((y_P*Z_{mQ})^2 - Y_{mQ}^2) - (X_{mQ} + x_P*Z_{mQ})*(X_{mQ} - x_P*Z_{mQ})^2
+
+	ZR.Mul(&ZmQ, &t0) // = Z_{mQ}*(X_{mQ} - x_P*Z_{mQ})^2
+
+	var xR ProjectivePoint
+	xR.x.a = XRa.a
+	xR.x.b = XRb.a
+	xR.z.a = ZR.a
+
+	return xR
+}
